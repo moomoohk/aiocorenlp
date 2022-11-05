e8888225dc08296be78484a2bab1e08ae64912f8
moomoohk <moomoohk2@gmail.com>
2022-11-06 01:44:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
+from pathlib import Path
 
 from aiocorenlp import __version__
+
+long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='aiocorenlp',
     version=__version__,
     packages=['aiocorenlp'],
-    url='',
+    url='https://github.com/moomoohk/aiocorenlp',
     license='MIT',
     author='moomoohk',
     description='Asyncio support for Stanford CoreNLP',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires=">=3.10",
     install_requires=[
         "nltk",
